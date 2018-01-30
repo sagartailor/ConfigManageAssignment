44f3dd028d048be18a7c075537cb03ad20cb72d0
Sagar Tailor <sagartailor@oakland.edu>
2018-01-29 19:27:28 -0500

    implementations branch added, including functionality code to the programs classes

--- src/main/java/com/example/lab1/BeerSong.java
+++ src/main/java/com/example/lab1/BeerSong.java
@@ -4,13 +4,28 @@ public class BeerSong {
 
     public static void main(String[] args) {
 
-        
+        Ninety_Nine_bottles_of_Beer();
+
+
 
 
 
     }
 
-        public class Ninety_Nine_bottles_of_Beer{}
+
+
+
+    private static void Ninety_Nine_bottles_of_Beer() {
+    for (int i = 99; i >=0; i--) {
+        if (i==99){System.out.println(i+" bottles of beer on the wall," +i+" bottles of beer");}
+        else if (i==0){System.out.println("take one down, pass it around, no more bottles of beer on the wall.");}
+        else {System.out.println("take one down, pass it around,"+i+" bottles of beer on the wall.");
+            System.out.println(i+" bottles of beer on the wall," +i+" bottles of beer");}
+
+
+
 
     }
+
+}
 }
